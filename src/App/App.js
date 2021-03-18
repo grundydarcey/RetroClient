579887048bfc7f71cdaef5ad805174eb4b65457c
Beyonce Knowles <you@example.com>
2021-03-18 13:36:04 -0400

    add favicon and title

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Solar from '../Images/SolarSystem.png';
+import { Route } from 'react-router-dom';
+import LandingPage from './LandingPage/landingpage';
 
 function App() {
   return (
     <main className='App'>
       <p>RETRODATE</p>
       <img src={Solar} alt='solarsystem' className='solarSystem'></img>
+      <Route exact path='/' component={LandingPage} />
     </main>
   );
 }
---
+++ src/App/LandingPage/landingpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './landingpage.css';
+
+export default class LandingPage extends React.Component {
+  render() {
+    return (
+      <section className='landing'>
+        <p>Landing Page</p>
+      </section>
+    )
+  }
+}
