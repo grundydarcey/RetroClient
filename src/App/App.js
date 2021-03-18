5d9491dfe09605c381322cde5d972b7824654908
Beyonce Knowles <you@example.com>
2021-03-18 13:16:26 -0400

    header done

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Solar from '../Images/SolarSystem.png';
 
 function App() {
   return (
     <main className='App'>
-      <p>Welcome to Retrograde</p>
+      <p>RETRODATE</p>
+      <img src={Solar} alt='solarsystem' className='solarSystem'></img>
     </main>
   );
 }
