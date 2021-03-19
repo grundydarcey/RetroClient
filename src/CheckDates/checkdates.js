982169f3b228f70e767679bf7a54b5554f5b9a85
Beyonce Knowles <you@example.com>
2021-03-19 16:28:03 -0400

    remove unnecessary h2

--- src/CheckDates/checkdates.js
+++ src/CheckDates/checkdates.js
@@ -21,7 +21,6 @@ export default class CheckDates extends Component {
   render() {
     return (
       <section className='futuredates'>
-        <h2>Check future dates</h2>
         <p>"By failing to prepare, you are preparing to fail." - Benjamin Franklin</p>
         <p>You really can't be too prepared or too in-the-know about your plans for the future. If you'll be outside all day, you're always better off checking the weather first. If you're planning a wedding, you'd want to know if a huge city-wide celebration was happening in the meantime.</p>
         <p>Different planets in retrograde can impact different parts of our lives, like romance, career, self-care and mental health. So use the tool beneath to enter a date in the future to get a personalized report for the status of the planets for that day.</p>
