b07a28869be1b92bd30d48d6fc424045c1f8d25d
Beyonce Knowles <you@example.com>
2021-03-20 11:45:04 -0400

    today fix

--- src/LandingPage/landingpage.js
+++ src/LandingPage/landingpage.js
@@ -9,7 +9,8 @@ export default class LandingPage extends React.Component {
     return (
       <section className='landing'>
         <p>It's a big world out there. Just because things go in their typical fashion here on Earth, doesn't mean they do on other planets. It's everyday we hear that some other planet is in retrograde.</p>
-        <p>These other planets we share the solar system with can occassionally appear to backtrack on their rotation. This is what is described as entering 'retrograde'. We know these planetary shifts can have effects on us here on Earth. Have you ever wanted to quickly and easily see the current retrograde status of the planets around us? Visit our links up top to see different features of the website.</p>
+        <p>These other planets we share the solar system with can occassionally appear to backtrack on their rotation. This is what is described as entering 'retrograde'. We know these planetary shifts can have effects on us here on Earth.</p>
+        <p>Have you ever wanted to quickly and easily see the current retrograde status of the planets around us? Or look at a date in the future and wonder what planets are going what way that day? Visit our links up top to see different features of the website.</p>
       </section>
     )
   }
